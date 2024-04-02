dc003559206a797550b51e807bf2a01902ddc8c1
marmila <64802282+marmila@users.noreply.github.com>
2024-04-02 18:41:20 +0200

    updated user-data commands for ubuntu

--- Two tier architecture in AWS using terraform/user-data.sh
+++ Two tier architecture in AWS using terraform/user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
 echo "<h1>Hello Viky from $(hostname -f)</h1>" > /var/www/html/index.html
