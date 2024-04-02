8dda48c188d4a463afaf447a8f238f225274b1d6
marmila <64802282+marmila@users.noreply.github.com>
2024-04-02 16:36:52 +0200

    fixed cyclic dependency error

--- Two tier architecture in AWS using terraform/sg.tf
+++ Two tier architecture in AWS using terraform/sg.tf
@@ -3,7 +3,6 @@ resource "aws_security_group" "sg" {
   description = "Allow tls for inbound traffic"
   vpc_id = aws_vpc.myvpc.id
 
-
   ingress {
     description      = "SSH from VPC"
     from_port        = 22
@@ -28,15 +27,6 @@ resource "aws_security_group" "sg" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    description      = "Rule to allow connections to database from any instances this security group is attached to"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.db-sg.id]
-    self             = false
   }
 
   tags = {
